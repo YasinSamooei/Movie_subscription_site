2d505e26d488a759c21a07f0e7e8a7350ae029d1
Abolfazl <abolfazlzaker.dev@gmail.com>
2023-07-24 00:07:15 +0330

    refactor: add random objects for suggested videos and optimize queries

--- blog/views.py
+++ blog/views.py
@@ -11,20 +11,19 @@ class BlogDetailView(HitCountDetailView):
     """
     View for blog detail view
     with comment and reply capability
-    """    
-    count_hit=True
-    model=Blog
+    """
+    count_hit = True
+    model = Blog
     slug_field = 'slug'
-    template_name="blog/blog_detail.html"
-    context_object_name="blog"
+    template_name = "blog/blog_detail.html"
+    context_object_name = "blog"
 
     def get_context_data(self, **kwargs):
         context = super(BlogDetailView, self).get_context_data(**kwargs)
 
         context['latest_blogs'] = Blog.objects.all()[:6]
-        context['tags']=Tag.objects.all()
+        context['tags'] = Tag.objects.all()
         return context
-
 
 
 class SearchView(ListView):
@@ -42,13 +41,12 @@ class BlogListView(ListView):
     template_name = "blog/blog_list.html"
     model = Blog
     paginate_by = 10
-    context_object_name="blogs"
-    
+    context_object_name = "blogs"
+
     def get_context_data(self, **kwargs):
         context = super(BlogListView, self).get_context_data(**kwargs)
-
-        context['latest_blogs'] = Blog.objects.all()[:6]
-        context['tags']=Tag.objects.all()
+        context['latest_blogs'] = self.object_list.all()[:6]
+        context['tags'] = Tag.objects.all()
         return context
 
 
@@ -56,12 +54,11 @@ class PopularBlogListView(ListView):
     template_name = "blog/blog_list.html"
     model = Blog
     paginate_by = 10
-    context_object_name="blogs"
-    queryset=Blog.objects.order_by('-hit_count_generic__hits')
+    context_object_name = "blogs"
+    queryset = Blog.objects.order_by('-hit_count_generic__hits')
 
     def get_context_data(self, **kwargs):
         context = super(PopularBlogListView, self).get_context_data(**kwargs)
-
         context['latest_blogs'] = Blog.objects.all()[:6]
-        context['tags']=Tag.objects.all()
+        context['tags'] = Tag.objects.all()
         return context
